e550c44c039a43267cf5fdabd71fe92bf841ac12
AlexandreBed <alexandre.bedard@hotmail.com>
2024-09-09 23:58:21 +0000

    Commentaire parent/enfant

--- fork2.c
+++ fork2.c
@@ -41,12 +41,17 @@ int main()
 {
     pid_t pid;
     pid = fork();
-
-    // Appel fonction Enfant
-
-
-    // Appel fonction Parent
-
-    
+if (pid < 0) {
+        // Erreur lors de la création du processus
+        perror("Erreur lors de fork");
+        exit(EXIT_FAILURE);
+    } else if (pid == 0) {
+        // Ceci est le code exécuté par le processus fils
+        codeDuProcessusEnfant();
+    } else {
+        // Ceci est le code exécuté par le processus père
+        wait(NULL);  // Attendre que le fils se termine
+        codeDuProcessusParent();
+    }
     return 0;
 }
